ffe4082fb1156897dd11e76b1c35a8ca60bdeb55
mfarid <mohd.farid@devfactory.com>
2017-02-27 20:04:14 +0530

    Add ActiveMQ Jms example

---
+++ src/main/java/guru/springframework/SpringBootActiveMQApplication.java
@@ -0,0 +1,45 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import javax.jms.ConnectionFactory;
+
+@SpringBootApplication
+@EnableJms
+public class SpringBootActiveMQApplication {
+
+    public final static String MAIL_QUEUE = "our-mail-queue";
+
+    @Bean
+    public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+      DefaultJmsListenerContainerFactoryConfigurer configurer) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        // This provides all boot's default to this factory, including the message converter
+        configurer.configure(factory, connectionFactory);
+        // You could still override some of Boot's default if necessary.
+        return factory;
+    }
+
+    @Bean // Serialize message content to json using TextMessage
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context =
+          SpringApplication.run(SpringBootActiveMQApplication.class, args);
+    }
+}
---
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -0,0 +1,106 @@
+package guru.springframework.controllers;
+
+import guru.springframework.SpringBootActiveMQApplication;
+import guru.springframework.commands.ProductForm;
+import guru.springframework.converters.ProductToProductForm;
+import guru.springframework.domain.Product;
+import guru.springframework.services.ProductService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by jt on 1/10/17.
+ */
+@Controller
+public class ProductController {
+
+    private static final Logger log = Logger.getLogger(ProductController.class);
+
+    private ProductService productService;
+
+    private ProductToProductForm productToProductForm;
+
+    private JmsTemplate jmsTemplate;
+
+    @Autowired
+    public void setProductToProductForm(ProductToProductForm productToProductForm, JmsTemplate jmsTemplate) {
+        this.productToProductForm = productToProductForm;
+        this.jmsTemplate = jmsTemplate;
+    }
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping("/")
+    public String redirToList(){
+        return "redirect:/product/list";
+    }
+
+    @RequestMapping({"/product/list", "/product"})
+    public String listProducts(Model model){
+        model.addAttribute("products", productService.listAll());
+        return "product/list";
+    }
+
+    @RequestMapping("/product/show/{id}")
+    public String getProduct(@PathVariable String id, Model model){
+        model.addAttribute("product", productService.getById(Long.valueOf(id)));
+        return "product/show";
+    }
+
+    @RequestMapping("product/edit/{id}")
+    public String edit(@PathVariable String id, Model model){
+        Product product = productService.getById(Long.valueOf(id));
+        ProductForm productForm = productToProductForm.convert(product);
+
+        model.addAttribute("productForm", productForm);
+        return "product/productform";
+    }
+
+    @RequestMapping("/product/new")
+    public String newProduct(Model model){
+        model.addAttribute("productForm", new ProductForm());
+        return "product/productform";
+    }
+
+    @RequestMapping(value = "/product", method = RequestMethod.POST)
+    public String saveOrUpdateProduct(@Valid ProductForm productForm, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            return "product/productform";
+        }
+
+        Product savedProduct = productService.saveOrUpdateProductForm(productForm);
+
+        return "redirect:/product/show/" + savedProduct.getId();
+    }
+
+    @RequestMapping("/product/delete/{id}")
+    public String delete(@PathVariable String id){
+        productService.delete(Long.valueOf(id));
+        return "redirect:/product/list";
+    }
+
+    @RequestMapping("/product/indexProduct/{id}")
+    public String indexProduct(@PathVariable String id){
+        Map<String, String> actionmap = new HashMap<>();
+        actionmap.put("id", id);
+        log.info("Sending the index request through queue message");
+        jmsTemplate.convertAndSend("our-mail-queue", actionmap);
+        return "redirect:/product/show/"+id;
+    }
+}
